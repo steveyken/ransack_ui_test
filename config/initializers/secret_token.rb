fefc059de5d7f42258f0c082cf309f30a50b7c34
Steve Kenworthy <steveyken@gmail.com>
2012-12-20 12:34:04 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RansackUiTest::Application.config.secret_token = '18bbc8682854dd4456bae4aec1be538dbda0ba703ad28ad071e22687717b9a69be33d6d42f5fe9db576f7b0ecf256548c675b669a33c671e1d9f75969b152cbf'
